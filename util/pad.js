d9f4af52bdad4727870a39f040353a9185e4c7e8
Andrew Chalmers <andrew.n.chalmers@gmail.com>
2014-11-20 00:22:15 -0500

    removed useless shit

--- util/pad.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (standardCallback, funct) {
-    try {
-      funct()
-    } catch (err) {
-    console.log(colors.red('Error determining if user exists!'));
-    console.log(err);
-    standardCallback(err, undefined);
-  }
-}
