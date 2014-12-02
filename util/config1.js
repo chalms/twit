44be6e6004482846c806a2159f97ef957e9d38f3
chalms <andrew.n.chalmers@gmail.com>
2014-12-02 02:00:25 -0500

    Delete config1.js

--- util/config1.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key:"UgyhukF92kgBbysTVadrD4jAh",
-  consumer_secret:"0ml5oumgBzazmxb0MSdcholxYe9Vl2TGwamJybjhyNwjrTBbHw",
-  access_token:"2557374872-jE4OyQ1jrMYkfOaU5QWKr9XyolTt9357uupueN8",
-  access_token_secret:"POvkagsQ2fWoQe70coR2QUXOs1r3dGNvG92SDifr6M4CY"
-}
